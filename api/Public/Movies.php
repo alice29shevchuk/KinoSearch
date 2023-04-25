3b1cf845156e7c29e44294eb3564e735bde7c680
alice29shevchuk <Sevc_sd76@student.itstep.org>
2023-04-25 16:24:42 +0300

    update function getByAuthor

--- api/Public/Movies.php
+++ api/Public/Movies.php
@@ -4,32 +4,37 @@ require_once 'C:\xampp\htdocs\KinoPoisk\db\Controllers\FilmController.php';
 require_once 'C:\xampp\htdocs\KinoPoisk\db\Controllers\MemberController.php';
 require_once 'C:\xampp\htdocs\KinoPoisk\db/db.php';
 if(isset($_GET['author'])){
-    getByAuthor();
-}
-function getByAuthor(){
-    $film =  new FilmController(ConnectWithDB::getInstance());
     $role = new RoleController(ConnectWithDB::getInstance());
     $listRole = $role->SelectByFilter(function ($x){return $x->title == "Author";});
-    if(count($listRole) != 0){
+    if(count($listRole) != 0) {
         $idRole = $listRole[0]->id;
-        $par = new MemberController(ConnectWithDB::getInstance());
-        $listPar = $par->SelectByFilter(function ($x) use ($idRole) {return $x->roleId == $idRole;});
-        foreach ($listPar as $item){
-            if($item->fullName == $_GET['author']){
-                $masFilms = $film->SelectByFilter(function ($x) use ($item) {return $x->memberId == $item->id;});
-                if( count($masFilms) > 0 ){
-                    $json = json_encode($masFilms);
-                    echo $json;
-                }
-                else{
-                    echo "400";
-                }
-
-            }
-            else{
-                echo "400";
-            }
+        $mem = new MemberController(ConnectWithDB::getInstance());
+        $listMember = $mem->SelectByFilter(function ($x) use ($idRole) {return $x->roleId == $idRole;});
+        $author = $listMember[0];
+        if(count(getByAuthor($author)) > 0){
+            echo json_encode(getByAuthor($author));
+        }
+        else{
+            echo "404";
         }
     }
+    else{
+        echo "404";
+    }
+}
+function getByAuthor($author):array
+{
+    $film = new FilmController(ConnectWithDB::getInstance());
+    if ($author->fullName == $_GET['author']) {
+        $masFilms = $film->SelectByFilter(function ($x) use ($author) {
+            return $x->memberId == $author->id;
+        });
+        if (count($masFilms) > 0) {
+            return $masFilms;
+        } else {
+            return [];
+        }
+    }
+    return [];
 }
 ?>
